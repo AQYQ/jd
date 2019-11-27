9e27ecb32ad1e022d431740b1769ee4e07023d7f
mason <1915595815@qq.com>
2019-11-27 17:51:32 +0800

    依赖用法

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import axios from 'axios'
+import ElementUi from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import store from './vuex/store.js'
+Vue.use(ElementUi);
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/vuex/store.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    //定义的字段
+    state:{
+
+    },
+    //改变字段需要的方法
+    mutations:{
+
+    },
+    //commit的方法
+    actions:{
+        
+    }
+})
+
+export default store;
