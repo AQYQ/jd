b4cd0b8aa8d9e8b53db50831edcca6b14c6f8a6c
zhaozhiqiang <zhaozhiqiang@honghe-tech.com>
2019-11-27 17:27:53 +0800

    头部公共组件

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="shortcut">
+        <div class="main">
+            <ul class="fl">
+                <li>北京</li>
+            </ul>
+            <ul class="fr"></ul>
+        </div>
+    </div>
+</template>
+<style>
+    #shortcut{
+        border-bottom: 1px solid #ddd;
+        background-color: #e3e4e5;
+    }
+    #shortcut .main{
+        height: 30px;
+        line-height: 30px;
+        color: #999;
+        width: 1190px;
+        margin: auto;
+    }
+    /* 左浮动的地点选择 */
+    .fl{
+        float: left;
+    }
+    /* 右浮动的菜单 */
+    .fr{
+        float: right;
+    }
+</style>
